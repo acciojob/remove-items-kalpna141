8a554b892d225df8d7593e2dcbc5f0d8898424be
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-06-30 15:44:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+ function removeColor() {
+      const colorSelect = document.getElementById('colorSelect');
+      const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+      
+      if (selectedOption) {
+        colorSelect.remove(selectedOption.index);
+      }
+    }
